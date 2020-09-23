244aa9fb1a00c534dba6b648e099c9cba5e6ea72
alecsmirnov <alecsmirnov93@gmail.com>
2020-09-23 21:51:18 +0700

    Added app logic

--- BullseyeUIKit/BullseyeUIKit/ViewController.swift
+++ BullseyeUIKit/BullseyeUIKit/ViewController.swift
@@ -8,25 +8,78 @@
 
 import UIKit
 
+private enum Settings {
+    static let minimumScore = 1
+    static let maximumScore = 100
+}
+
 class ViewController: UIViewController {
-    private var currentValue = 50
+    private var currentValue = 0
+    private var targetValue = 0
+    private var score = 0
+    private var round = 1
+    
+    @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
+    
+    @IBOutlet private weak var slider: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        startNewGame()
+    }
+    
+    private func resetCurrentValueAndTargetValue() {
+        currentValue = (Settings.minimumScore + Settings.maximumScore) / 2
+        targetValue = Int.random(in: Settings.minimumScore...Settings.maximumScore)
+    }
+    
+    private func startNewGame() {
+        resetCurrentValueAndTargetValue()
+        
+        slider.value = Float(currentValue)
+        
+        updateLabels()
+    }
+    
+    private func startNewRound() {
+        resetCurrentValueAndTargetValue()
+        
+        slider.value = Float.random(in: Float(Settings.minimumScore)...Float(Settings.maximumScore))
+        round += 1
+        
+        updateLabels()
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
     }
 
-    @IBAction func sliderMoved(_ slider: UISlider) {
+    @IBAction private func sliderMoved(_ slider: UISlider) {
         currentValue = lroundf(slider.value)
     }
     
-    @IBAction func showAlert() {
-        let message = "The value of the slider is: \(currentValue)"
+    @IBAction private func showAlert() {
+        let difference = abs(targetValue - currentValue)
+        let points = 100 - difference
+        
+        score += points
+        
+        let message = "The value of the slider is: \(currentValue)\n" +
+                      "The target value is: \(targetValue)\n" +
+                      "You scored \(points) points"
         
         let alert = UIAlertController(title: "Perfect!", message: message, preferredStyle: .alert)
         let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
         
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        
+        startNewRound()
     }
 }
 
