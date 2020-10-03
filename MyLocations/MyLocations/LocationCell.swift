2c61c71c962479f3addcbbaee5b4a35fad3dc106
alecsmirnov <alecsmirnov93@gmail.com>
2020-10-03 19:15:21 +0700

    Added thumbnails and image size optimization

--- MyLocations/MyLocations/LocationCell.swift
+++ MyLocations/MyLocations/LocationCell.swift
@@ -11,7 +11,8 @@ import UIKit
 class LocationCell: UITableViewCell {
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var addressLabel: UILabel!
-
+    @IBOutlet weak var photoImageView: UIImageView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
     }
@@ -44,5 +45,15 @@ class LocationCell: UITableViewCell {
         } else {
             addressLabel.text = String(format: "Lat: %.8f, Long: %.8f", location.latitude, location.longitude)
         }
+        
+        photoImageView.image = thumbnail(for: location)
+    }
+    
+    func thumbnail(for location: Location) -> UIImage {
+        if location.hasPhoto, let image = location.photoImage {
+            return image.resized(withBounds: CGSize(width: 52, height: 52))
+        }
+        
+        return UIImage()
     }
 }
