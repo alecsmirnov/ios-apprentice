ddbcca158d8a728f743109dca946d4d45aaf79ca
alecsmirnov <alecsmirnov93@gmail.com>
2020-10-04 15:09:07 +0700

    StoreSearch initial

---
+++ StoreSearch/StoreSearch/SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchViewController.swift
+//  StoreSearch
+//
+//  Created by Admin on 04.10.2020.
+//  Copyright © 2020 alec. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
