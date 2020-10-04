0c78016562b4ca45d47a973d62064b8c96c4edc5
alecsmirnov <alecsmirnov93@gmail.com>
2020-10-04 19:29:24 +0700

    Style the search bar and add app icons

--- StoreSearch/StoreSearch/SceneDelegate.swift
+++ StoreSearch/StoreSearch/SceneDelegate.swift
@@ -16,6 +16,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        
+        customizeAppearance()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -44,5 +46,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
+    }
+    
+    // MARK: - Helper Methods
+    
+    func customizeAppearance() {
+        let barTintColor = UIColor(red: 20 / 255, green: 160 / 255, blue: 160 / 255, alpha: 1)
+        
+        UISearchBar.appearance().barTintColor = barTintColor
     }
 }
